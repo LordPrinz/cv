b8732a4a6c9d42d87a649a99be6783928220d588
LordPrinz <plazinski2902@gmail.com>
2024-03-06 00:42:32 +0100

    Create dynamic rodo paragraph

--- src/components/rodo-paragraph.tsx
+++ src/components/rodo-paragraph.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
+
 const RodoParagraph = () => {
+  const searchParams = useSearchParams();
+  const companyName = searchParams.get("companyName") || "(nazwa firmy)";
+
   return (
     <p className="absolute bottom-0  hidden max-w-sm self-end text-[10px] text-muted-foreground print:block">
-      Wyrażam zgodę na przetwarzanie moich danych osobowych przez (nazwa firmy)
+      Wyrażam zgodę na przetwarzanie moich danych osobowych przez {companyName}{" "}
       w celu prowadzenia rekrutacji na aplikowane przeze mnie stanowisko.
     </p>
   );

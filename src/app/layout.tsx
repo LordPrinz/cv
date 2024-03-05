2be28b1b97fc7599eb50349029ed90705780ed40
LordPrinz <plazinski2902@gmail.com>
2024-03-05 23:25:19 +0100

    Create metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,44 @@ import "./globals.css";
 import React from "react";
 
 import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import { RESUME_DATA } from "@/data/resume-data";
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
   description: RESUME_DATA.summary,
+  authors: [
+    {
+      name: "Oskar Płaziński",
+      url: "https://github.com/LordPrinz",
+    },
+  ],
+  keywords: [
+    "Oskar Płaziński",
+    "LordPrinz",
+    "Full Stack Developer",
+    "Software Technician",
+    "Gliwice",
+    "Poland",
+    "Łany",
+    "Łany, Poland",
+    "ZSTI",
+    "Zespół Szkół Techniczno-Informatycznych",
+    "Gliwicki Laur Nauki",
+    "Silesian CYBERhackathon",
+    "Motorola Science Cup",
+    "Dzajlopment",
+    "Dzajde",
+    "Dzajco",
+  ],
+  openGraph: {
+    siteName: "Oskar Płaziński | Full Stack Developer",
+    description: RESUME_DATA.summary,
+    type: "profile",
+    images: "/open-graph.png",
+  },
+  creator: "Oskar Płaziński",
+  publisher: "Oskar Płaziński",
+  metadataBase: new URL("https://cv.dzaj.de"),
 };
 
 export default function RootLayout({
